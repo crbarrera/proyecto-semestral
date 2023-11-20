882c8491bcd88d2962140dfaab474139aa52541d
crbarrera <cr.barrera@duocuc.cl>
2023-11-20 11:24:50 -0300

    Proyecto finalizado

--- PelisBlog/Peliculas/admin.py
+++ PelisBlog/Peliculas/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Peliculas, PerfilUsuario
+from .models import Peliculas, PerfilUsuario, Factura
 # Register your models here.
 admin.site.register(Peliculas)
 admin.site.register(PerfilUsuario)
+admin.site.register(Factura)
--- PelisBlog/Peliculas/views.py
+++ PelisBlog/Peliculas/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
-from Peliculas.models import Peliculas, PerfilUsuario
+from django.shortcuts import render, get_object_or_404, redirect
+from Peliculas.models import Peliculas, PerfilUsuario, Factura
+
 # Create your views here.
 
 def lista_peliculas(request):
@@ -9,7 +10,26 @@ def lista_peliculas(request):
 def ficha_pelicula(request, id):
     
     pelicula = Peliculas.objects.get(id=id)
-    
+    if request.method == 'POST':
+        if request.user.is_authenticated:
+            if not request.user.is_staff:
+                user = request.user
+                
+                perfil = PerfilUsuario.objects.get(user=user)
+                
+                usuario = perfil
+                
+                nombre_pelicula = pelicula.nombre
+                precio_pelicula = pelicula.precio
+                cantidad_pelicula = request.POST['cantidad']
+                
+                factura = Factura.objects.create(usuario=usuario, nombre_producto=nombre_pelicula, monto_producto=cantidad_pelicula, precio_producto=precio_pelicula)
+                return redirect ('orden_de_compra', factura.pk)
+            else:
+                return redirect ('ficha_pelicula', id = pelicula.pk)
+        else:
+            return redirect ('ficha_pelicula', id = pelicula.pk)
+            
     return render(request, 'ficha_pelicula.html', {'pelicula':pelicula})
 
 def compra_exitosa(request):
@@ -17,4 +37,13 @@ def compra_exitosa(request):
 
 def compras(request):
     persona = PerfilUsuario.objects.all()
+    if request.method == "POST":
+        valor_id = request.POST['factura_id']
+        factura_cambio = request.POST['estado']
+        
+        obtener_factura = get_object_or_404(Factura, pk=valor_id)
+        obtener_factura.estado_factura = factura_cambio
+        
+        obtener_factura.save()
     return render(request, 'compras.html', {'persona':persona})
+
